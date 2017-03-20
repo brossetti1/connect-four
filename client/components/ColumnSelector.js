8a1e6a1503321e67199c6335fbfcfe660377a9b4
brossetti1 <brossetti1@gmail.com>
2017-03-20 01:08:16 -0400

    adding data-column to column selector

--- client/components/ColumnSelector.js
+++ client/components/ColumnSelector.js
@@ -5,13 +5,13 @@ export default class ColumnSelector extends React.Component {
   render() {
     return (
       <div className="board__row">
-        <div className="board__column-selector"></div>
-        <div className="board__column-selector"></div>
-        <div className="board__column-selector"></div>
-        <div className="board__column-selector"></div>
-        <div className="board__column-selector"></div>
-        <div className="board__column-selector"></div>
-        <div className="board__column-selector"></div>
+        <div className="board__column-selector" data-column='1'></div>
+        <div className="board__column-selector" data-column='2'></div>
+        <div className="board__column-selector" data-column='3'></div>
+        <div className="board__column-selector" data-column='4'></div>
+        <div className="board__column-selector" data-column='5'></div>
+        <div className="board__column-selector" data-column='6'></div>
+        <div className="board__column-selector" data-column='7'></div>
       </div>
     );
   }
