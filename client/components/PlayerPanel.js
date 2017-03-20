a75bb61b0dd787c85eb4b643e3f6f49c7c5aa396
brossetti1 <brossetti1@gmail.com>
2017-03-20 00:06:04 -0400

    setting up the ability to rename the players on the Player Panel with react-edit-inline, adding style to edit-inline input

--- client/components/PlayerPanel.js
+++ client/components/PlayerPanel.js
@@ -1,21 +1,50 @@
 import React from 'react'
+import InlineEdit from 'react-edit-inline'
 
 export default class PlayerPanel extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.dataChanged = this.dataChanged.bind(this);
+    this.state = {
+      player_one_name: 'Player One',
+      player_two_name: 'Player Two',
+    }
+  }
+
+  dataChanged(data) {
+    this.setState(data)
+  }
+
   render() {
+
     return(
       <div className="players__container">
         <div className="players__player-one">
-          <h1>Player One</h1>
+          <InlineEdit
+            className="players__name"
+            activeClassName="players__editing"
+            text={this.state.player_one_name}
+            paramName="player_one_name"
+            change={this.dataChanged}
+          />
         </div>
+
         <div className="players__turn">
           <h3>Player One's turn</h3>
           <span>👈</span>
         </div>
-        <div className="players__player-two">
-          <h1>Player Two</h1>
-        </div>
+
+      <div className="players__player-two">
+        <InlineEdit
+          className="players__name"
+          activeClassName="players__editing"
+          text={this.state.player_two_name}
+          paramName="player_two_name"
+          change={this.dataChanged}
+        />
       </div>
+    </div>
     )
   }
 }
