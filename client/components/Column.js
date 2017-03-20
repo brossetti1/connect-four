cef69cb8eba8df521b62c80ecba721ef9d4e58c6
brossetti1 <brossetti1@gmail.com>
2017-03-20 01:05:46 -0400

    setting the id on the column for reference on adding tokens

--- client/components/Column.js
+++ client/components/Column.js
@@ -3,8 +3,9 @@ import React from 'react'
 class Column extends React.Component {
 
   render() {
+    const columnId = `column-${this.props.column}`
     return (
-      <div className="board__column">
+      <div className="board__column" id={columnId}>
         <div className="board__column-row"></div>
         <div className="board__column-row"></div>
         <div className="board__column-row"></div>
