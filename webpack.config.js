8935668ba49070ba06d8525fb0b231803728a899
brossetti1 <brossetti1@gmail.com>
2017-03-19 22:48:08 -0400

    adding web pack config for es6-promise, adding es6promise and query-string for constructing api calls

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,17 @@ module.exports = {
     path: path.join('app', 'assets', 'javascripts', 'react'),
     filename: "connect4_bundle.js",
   },
+  devtool: 'source-map',
   plugins: [
     new webpack.DefinePlugin({
       "process.env": { 
         NODE_ENV: JSON.stringify("production") 
       }
+    }),
+    // https://gist.github.com/Couto/b29676dd1ab8714a818f#gistcomment-1584602 - es6-promise
+    new webpack.ProvidePlugin({
+      'Promise': 'es6-promise',
+      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
     })
   ],
   module: {
